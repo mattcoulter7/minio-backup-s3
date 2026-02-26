3972a1cbcf5be780b626add51cc9825fec7b220a
Matthew Coulter <53892067+mattcoulter7@users.noreply.github.com>
2026-02-26 13:32:02 +1100

    Bump Docker image tag to 2.0.0
    Update bin/deploy.sh to build and push the Docker image with the 2.0.0 tag (alongside latest). This prepares the repository to release version 2.0.0 by updating the tag used in both the docker build and docker push commands.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,4 +1,4 @@
-docker build -f Dockerfile -t mattcoulter7/minio-backup-s3:latest -t mattcoulter7/minio-backup-s3:1.0.0 .
+docker build -f Dockerfile -t mattcoulter7/minio-backup-s3:latest -t mattcoulter7/minio-backup-s3:2.0.0 .
 docker login
-docker push mattcoulter7/minio-backup-s3:1.0.0
+docker push mattcoulter7/minio-backup-s3:2.0.0
 docker push mattcoulter7/minio-backup-s3:latest
